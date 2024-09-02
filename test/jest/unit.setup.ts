548ded952e46e26928cb518a108d802a6055a7f5
Bator Horvath <5924171+bhorvath@users.noreply.github.com>
2024-09-02 14:52:09 +1000

    Prepare jest for unit testing with msw

---
+++ test/jest/unit.setup.ts
@@ -0,0 +1,13 @@
+import { server } from "../mocks/msw/server";
+
+beforeAll(async () => {
+  server.listen({ onUnhandledRequest: "error" });
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
