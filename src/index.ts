6ddd41da0a4a1975cdbe67b20900221c289c4434
Bator Horvath <5924171+bhorvath@users.noreply.github.com>
2024-09-03 11:28:02 +1000

    Add index.ts with public exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { TomTomClient } from "./client";
+export * from "./types/client";
+export { ApiError } from "./errors/api";
+export { ApplicationError } from "./errors/application";
+export { InvalidStateError } from "./errors/invalid-state";
+export { ValidationError } from "./errors/validation";
